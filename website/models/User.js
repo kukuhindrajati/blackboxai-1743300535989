8733f3ef363d59672410dc2b9aa81ae212447803
kukuhindrajati <57124424+kukuhindrajati@users.noreply.github.com>
2025-03-30 09:09:43 +0700

    Add website/models/User.js 1743300584448

---
+++ website/models/User.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      set(value) {
+        const salt = bcrypt.genSaltSync(10);
+        this.setDataValue('password', bcrypt.hashSync(value, salt));
+      }
+    }
+  });
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  return User;
+};
